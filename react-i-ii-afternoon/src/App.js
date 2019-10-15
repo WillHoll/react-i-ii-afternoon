455198926c739996a813b13730c2765daddd6cd3
WillHoll <willhollingsworth15@yahoo.com>
2019-10-15 13:45:41 -0600

    initial setup

---
+++ react-i-ii-afternoon/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import data from './component/data';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
